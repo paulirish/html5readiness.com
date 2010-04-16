153287e580db594332379c47021acebab5ee52d3
Paul Irish <paul.irish@gmail.com>
2010-04-16 17:45:00 -0400

    small tweaks

--- js/script.js
+++ js/script.js
@@ -1,20 +1,22 @@
 jQuery(document).bind('DOMMouseScroll mousewheel', function(e, delta) {
 
-    return();
+    //return();
     var newval,
         num = $('div.css-chart p').css('padding-left');
 
     delta = delta || e.detail || e.wheelDelta;
 
     if (delta > 0 || e.which == 38) {
-        newval = parseFloat(num) + 5 * (e.shiftKey ? .1 : 1);
+        newval = parseFloat(num) + 10 * (e.shiftKey ? .1 : 1);
     } else if (delta < 0 || e.which == 40) {
-        newval = parseFloat(num) - 5 * (e.shiftKey ? .1 : 1);
+        newval = parseFloat(num) - 10 * (e.shiftKey ? .1 : 1);
     } else {
         return true;
     }
-
-    $('div.css-chart p').css('padding-left',newval + 'px');
+    
+    $('style.padleft').remove();
+    $('<style class="padleft"> div.css-chart p { padding-left : '+newval+'px; }</style>')
+        .appendTo(document.body);
 
     e.preventDefault();
 
